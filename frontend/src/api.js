2303fd45880737920fd363075d2fd1adbafd76e2
vibhuTripathi <amtri2030@gmail.com>
2025-12-19 15:52:12 +0530

    baseurl updated

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://p6xckmgk-3000.inc1.devtunnels.ms/api';
+// const API_BASE_URL = 'https://p6xckmgk-3000.inc1.devtunnels.ms/api';
+const API_BASE_URL = 'http://localhost:3000/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
